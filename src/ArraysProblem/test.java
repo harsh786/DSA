630f7ebf45f8f34580ad93e798ba10bb41ad11f6
Harsh Kumar (12204) <harsh.kumar01+pinegit@pinelabs.com>
2025-08-21 03:02:54 +0530

    test commit

---
+++ src/ArraysProblem/test.java
@@ -0,0 +1,10 @@
+package ArraysProblem;
+
+public class test {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
